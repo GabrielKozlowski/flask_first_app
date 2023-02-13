90e6cb4f14ff85fff20d2722c451d9341a2e9053
Gabrielkozlowski <gabriel89krk@gmail.com>
2023-02-13 13:00:25 +0100

    add class for create Form name/submit, new navbar button 'name'

--- hello.py
+++ hello.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "my super key"
+
+# Create a Form Class
+
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name:", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # Create a route decorator
 @app.route('/')
@@ -26,7 +37,7 @@ def user(name):
 # Create Custom Error Pages
 
 # Invalid URL
-#Error 404
+# Error 404
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
@@ -37,5 +48,23 @@ def page_not_found(e):
     return render_template("500.html"), 500
 
 
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template('name.html',
+        name = name,
+        form = form)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
